e3d3f3e574258ea6148abc8ade302945a2caa2ce
rt09 <78974453+rt09@users.noreply.github.com>
2024-01-11 13:30:42 +0530

    Update workoutdetails.js

--- src/components/workoutdetails.js
+++ src/components/workoutdetails.js
@@ -1,9 +1,25 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutContext"
 const Workoutdetails = ({ workout }) => {
+    const { dispatch } = useWorkoutsContext();
+    const handleclick = async () => {
+        
+        const response = await fetch('/api/workouts/' + workout._id, {
+            method:'DELETE'
+        })
+
+        const json = await response.json()
+
+        if (response.ok) {
+            dispatch({type:'DELETE_WORKOUT', payload:json})
+        }
+
+    }
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
-            <p><strong>Age (in Years):</strong>{workout.Age}</p>
-            <p><strong>Weight (in kg):</strong>{workout.Weight}</p>
+            <p><strong>Description: </strong>{workout.description}</p>
+            <p><strong>Date: </strong>{workout.date}</p>
+            <span onClick={handleclick}>delete</span>
         </div>
     )
 }
